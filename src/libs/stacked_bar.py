382860d19717ec062b3be0ff88fedb693f7200cf
chengshusss <chengshu_seu@163.com>
2021-07-30 00:29:34 +0800

    add success time distribution

---
+++ src/libs/stacked_bar.py
@@ -0,0 +1,66 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+"""
+src: https://blog.csdn.net/m0_45209371/article/details/108241200
+
+"""
+
+import matplotlib.pyplot as plt
+import random
+
+
+def plot_stacked_bar(data, config):
+    # 加载配置
+    width = config.get("width", 0.8)
+    title = config.get("title", "default title")
+    color_map_name = config.get("color_map_name", "tab20")
+    color_map = plt.get_cmap(color_map_name).colors
+
+    # 每次堆叠时的基准：buttom和color_map
+    pos = [0] * max([len(data[k]) for k in data.keys()])
+    color_index = 0
+    for label, line in data.items():
+        # 获取每行的值
+        values = list(line.values())
+        plt.bar(line.keys(), values, width, bottom = pos, label = label, color=color_map[color_index])
+
+        # 更新基准值
+        pos = [pos[i] + values[i] for i in range(len(values))]
+        color_index += 1
+
+    # 设置题目和图例
+    plt.title(title)
+    if config.get("legend", True):
+        plt.legend()
+
+    plt.show()
+
+
+def main():
+    # 配置绘图细节
+    config = {
+        "width": 0.8,
+        "title": "Temp for stacked bar",
+        "color_map_name": "tab20c",
+    }
+
+    # 生成假数据
+    data = {}
+    series_count = 4
+    data_length = 10
+    series = [f"series-{x}" for x in range(series_count)]
+    x_label = [f"7-{x+1}" for x in range(data_length)]
+    for item in series:
+        temp = {}
+        for label in x_label:
+            temp[label] = random.randint(5, 20)
+
+        data[item] = temp
+    # 自定义数据，请取消注释下一行，参考data变量的数据结构
+    # print(data)
+
+    # 绘图
+    plot_stacked_bar(data, config)
+
+if (__name__ == "__main__"):
+    main()
---
+++ src/which-time-success/main.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+import os
+import sys
+
+from matplotlib.pyplot import plot
+sys.path.append(os.path.join(os.path.dirname(__file__), '../libs'))
+from stacked_bar import plot_stacked_bar
+# from ..libs.stacked_bar import plot_stacked_bar
+
+def get_next(p: int, distribution: list):
+    """Return next round distribution.
+
+    Distribution represents the success times distribution after certain 
+    round, assuming this game is independent repeated experiment.
+    Given distribution in this round and the probability of success,
+    return the distribution in next round.
+
+    Args:
+        p: The probability of success.
+        distribution: The distribution of this round.
+
+    Returns:
+        A list contains the distribution of next round.
+        res[i] represent the probability of having i+1 times success.
+
+        [0.125, 0.375, 0.375, 0.125]
+
+    Raises:
+        AssertError: when distribution is empty, or p exceeds allowed range.
+    """
+
+    assert len(distribution) > 0, "Distribution is Empty."
+    assert 0 <= p <= 1, "p is more than 1 or less than 0."
+
+    res = []
+    for i in range(len(distribution)):
+        if i == 0:
+            # the first
+            res.append(distribution[i] * (1-p))
+        else:
+            res.append(distribution[i-1] * p + distribution[i] * (1-p))
+
+        if i == len(distribution) - 1:
+            res.append(distribution[i] * p)
+
+    return res
+
+
+def get_process(p, round):
+    distributions = [[1]]
+
+    for i in range(1, round):
+        temp = get_next(p, distributions[-1])
+        distributions.append(temp)
+    
+    return distributions
+
+
+def format_data(distributions):
+    data = {}
+    max_length = max([len(x) for x in distributions])
+    for i in range(len(distributions)):
+        for j in range(max_length):
+            if j not in data:
+                data[(j)] = {i:0}
+            else:
+                data[j][i] = 0
+    for i in range(len(distributions)):
+        for j in range(len(distributions[i])):
+                data[j][i] = distributions[i][j]
+    
+    return data
+
+
+
+
+def main():
+    config = {
+        "width": 0.8,
+        "title": "Success Time Distribution",
+        "color_map_name": "tab20c",
+        "legend": False
+    }
+    distributions = get_process(0.5, 20)
+    data = format_data(distributions)
+    # print(data)
+    plot_stacked_bar(data, config)
+
+if (__name__ == "__main__"):
+    main()
